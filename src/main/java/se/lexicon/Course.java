e16574ee88df254fd43fbaeeaed57c333c4e844d
Zhanti Stas <staszhanti@gmail.com>
2025-11-13 10:06:04 +0100

    created all the necessary classes such as Student, Course, StudentDaoImpl, CourseDaoImpl, and interfaces, also added basic logic to the Student and Course classes

---
+++ src/main/java/se/lexicon/Course.java
@@ -0,0 +1,82 @@
+package se.lexicon;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Course {
+    private int id;
+    private String courseName;
+    private LocalDate date;
+    private int weekDuration;
+    private Student student;
+
+    List<Student> students = new ArrayList<>();
+
+
+
+    public Course(int id, String courseName, LocalDate date, int weekDuration, Student student) {
+        setId(id);
+        setCourseName(courseName);
+        setDate(date);
+        setWeekDuration(weekDuration);
+    }
+
+
+    //Setter and Getters
+    //Id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        if(id !=0){
+            this.id = id;
+        }
+    }
+
+
+    //Course Name
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        if(courseName != null){
+            this.courseName = courseName;
+        }
+    }
+
+    //Date
+    public LocalDate getDate() {
+        return date;
+    }
+
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+
+    //Week Duration
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+    public void setWeekDuration(int weekDuration) {
+        if(weekDuration != 0|| weekDuration <= -1){
+            this.weekDuration = weekDuration;
+        }
+    }
+
+
+    //Student
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+}
---
+++ src/main/java/se/lexicon/Student.java
@@ -0,0 +1,69 @@
+package se.lexicon;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+
+
+    //Constructor
+    public Student(int id, String name, String email, String address) {
+        setid(id);
+        setName(name);
+        setEmail(email);
+        setAddress(address);
+    }
+
+
+
+
+    //Getter and Setter
+    //id
+    public int getid() {
+        return id;
+    }
+
+    public void setid(int id) {
+        if(id != 0 || id < 0) {
+            this.id = id;
+        }
+    }
+
+
+    //Name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name != null) {
+            this.name = name;
+        }
+    }
+
+
+    //email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if(email != null) {
+            this.email = email;
+        }
+    }
+
+
+    //address
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        if(address != null) {
+            this.address = address;
+        }
+    }
+}
